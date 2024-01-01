1460f5f9cfb8f3d5ab19e3be3fcfcde233e129f3
yogeshsahu04 <53236470+yogeshsahu04@users.noreply.github.com>
2024-01-02 02:22:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+#from flask import Flask
+#app = Flask(__name__)
+
+#@app.route('/')
+#def index():
+    #return 'Hello World , This is my first Application'
+#if __name__ == '__main__':
+      #app.run(host='0.0.0.0', port=8080)
+
+import os
+
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World , This is my first Application'
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8080)
+
+@app.route("/")
+def hello_world():
+    return "Hello world!! I hope you liked it"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
